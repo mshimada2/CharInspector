acd632217bb5664e4b2386062405a047cb00b8f5
mshimada <mshimada2@mail.csuchico.edu>
2018-02-07 18:12:36 -0800

    final version

--- main.cpp
+++ main.cpp
@@ -1,24 +1,41 @@
-//Author:
+//Author: McDermott Liam Shimada 
 #include<iostream>
 
 using namespace std;
+
+char letter;
 
 int main()
 {
 
   cout<<"What character do you want to know about?\n";
+  cin>>letter;
 
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+ 
+ if ('A' <= letter && letter <= 'Z'){
+
+ cout<<letter<<" is an upper case letter!\n";
+
+    }
+
+    else if ('a' <= letter && letter <= 'z'){
 
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+  cout<<letter<<" is a lower case letter!\n";
 
-  //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
+}
+ 
+ else {
+
+ //in all other cases...
+  cout<<letter<<" ?! Pssh. What are you talking about?\n";
+  
+}
+
   
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<(int) letter<<endl;
 
   return 0;
 }
